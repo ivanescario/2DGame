28b21637df5eeb3590b9bd94c81ac9929375604d
Ivan <iescario149@gmail.com>
2017-10-24 15:27:10 +0200

    Unity Project

---
+++ Assets/Scripts/Collisions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collisions : MonoBehaviour
+{
+    [Header("Physics2D.OverlapBox")]
+    public Vector2 boxPos;
+    public Vector2 boxSize;
+    public LayerMask mask;
+    [Header("Physics2D.OverlapCollider")]
+    public Collider2D col;
+    public ContactFilter2D filter;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Collider2D[] results = new Collider2D[5];
+        int numHits = Physics2D.OverlapBox(this.transform.position, boxSize, 0, filter, results);
+        if(numHits > 0) Debug.Log("Se ha detectado un collider"); // Si collider no esta vacío
+
+        //int n = Physics2D.OverlapCollider(col, filter, results);
+        //Physics2D.OverlapCollider(col, filter, results);
+    }
+
+    private void OnDrawGizmosSelected() // Solo dibuja los gizmos de los objetos que están seleccionados
+    {
+        Gizmos.DrawWireCube(this.transform.position, boxSize);
+    }
+}
---
+++ Assets/Scripts/Editor/CollisionInspector.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Collisions2))]
+public class CollisionInspector : Editor
+{
+    static bool stateFoldout;
+    static bool drawDefaultInspector;
+
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI(); // Si se deja escrito, no afecta. Se visualiza de manera normal la información en el inspector.
+        Collisions2 col = (Collisions2)target;
+
+        GUIStyle booleanText = new GUIStyle();
+
+        EditorGUILayout.Space();
+
+        EditorGUI.indentLevel = 1; // La posición en X que desplaza el botón
+        stateFoldout = EditorGUILayout.Foldout(stateFoldout, "State", true, EditorStyles.toolbarDropDown);
+
+        if(stateFoldout)
+        {
+            EditorGUILayout.Space(); // Aplica un espacio entre el texto anterior escrito y el siguiente.
+            EditorGUILayout.BeginVertical(EditorStyles.textArea);
+
+            EditorGUI.indentLevel = 2; // La posición en X que desplaza el texto 
+
+            if(col.isGrounded) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Is Grounded", booleanText);
+            if(col.wasGroundedLastFrame) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Was Grounded Last Frame", booleanText);
+            if(col.justGotGrounded) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Just Got Grounded", booleanText);
+            if(col.justNOTGrounded) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Just NOT Grounded", booleanText);
+            if(col.isFalling) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Is Falling", booleanText);
+            if(col.isWall) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Is Wall", booleanText);
+            if (col.wasWallLastFrame) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Was Wall Last Frame", booleanText);
+            if (col.justGotWall) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Just Got Wall", booleanText);
+            if (col.justNotWall) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Just Not Wall", booleanText);
+            if (col.isCeil) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Is Ceil", booleanText);
+            if (col.wasCeilLastFrame) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Was Ceil Last Frame", booleanText);
+            if (col.justGotCeil) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Just Got Ceil", booleanText);
+            if (col.justNotCeil) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Just NOT Ceil", booleanText);
+
+            EditorGUILayout.EndVertical();
+        }
+
+        EditorGUILayout.Space();
+
+        EditorGUI.indentLevel = 1;
+
+        drawDefaultInspector = EditorGUILayout.Foldout(drawDefaultInspector, "Default Inspector", true, EditorStyles.toolbarDropDown);
+
+        if(drawDefaultInspector)
+        {
+            EditorGUILayout.Space();
+
+            EditorGUI.indentLevel = 2;
+            EditorGUILayout.Space();
+        }
+    }
+}
